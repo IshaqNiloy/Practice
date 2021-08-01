9613d187e31405c806257b5f409ac2df57c63988
Ishaq Ali <ishaqniloy1996@gmail.com>
2021-07-31 22:38:46 -0700

    Added the documentation.

--- Append and Delete.py
+++ Append and Delete.py
@@ -21,24 +21,36 @@ def appendAndDelete(s, t, k):
     counter = 0
     flag = 0
     
+    #Case 1
     if s == t:
         return 'Yes'
+    
+    #Case 2
     elif set(s) == set(t) and abs(len(s) - len(t)) + 2 == k:
         return 'Yes'
+    #Case 3
     else:
+        #We need to go to the end of the longest string. That is why we are taking the maximum length between two strings.
         if len(s) > len(t):
             length = len(s)
         else:
             length = len(t)
+        #We need to compare each letter of the two strings index wise.
         for i in range(length):
+        #We need to use a try except block here because the length of the two strings is not equal.
+        #If the letters of the two strings are equal for the same location then we do not need to do anything.
+        #We are taking a flag here because we need to continue until the substring of both the strings is same. Here the substring starts from the beginning of the string.
             try:
                 if s[i] == t[i] and flag == 0:
                     continue
+        #If the letters are not equal then we need to add 2 to the counter. One for delete and one for append.
                 else:
                     counter += 2
                     flag = 1
+        #We need to add 1 to the counter for each letter of the rest of the longest string. This is for the deletion.
             except:
                 counter += 1
+        #If the counter is equal to k or k is greater than the sum of the length of the two strings then we return a 'Yes'. We return 'Yes' if k is greater than the sum of the length of two strings because we can waste extra moves by deletion. By doing multiple deletions we will get an empty string. 
         if counter == k or k > (len(s) +len(t)):
             return 'Yes'
         else:
